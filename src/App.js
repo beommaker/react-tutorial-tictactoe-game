e896c718ae27c261589f0a61a920cfe32caee026
beommaker <beom.maker@gmail.com>
2017-12-01 14:15:27 +0900

    feat(tictactoe-game): add-Game-structure
    Game Structure and first step(square)
    
    https://app.asana.com/0/483222130713609/483299025496303

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game.jsx'
+
 
 class App extends Component {
   render() {
@@ -8,11 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">tic-tac-toe GAME!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-main">
+          <Game />
+        </main>
       </div>
     );
   }
