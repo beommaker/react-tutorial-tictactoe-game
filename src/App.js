bd1ad1273255d462b8ca17c63d682f4109f61cd7
beommaker <beom.maker@gmail.com>
2017-12-01 19:31:26 +0900

    feat(tictactoe-game): need-to-solve-problem
    https://app.asana.com/0/483222130713609/492700708697957

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Game from './Game.jsx'
+import Game from './Game.js'
 
 
 class App extends Component {
